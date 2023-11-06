01db5e1c48bb3d01e858d4e41cabe938bb4d880d
Lucas <lucas.batista20012007@gmail.com>
2023-11-06 16:51:54 -0400

    first commit

---
+++ trabalho/Funcionario .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho
+{
+    internal class Funcionario
+    {
+        public string nome { get; set; }
+        public string endererco { get; set; }
+        public int cpf { get; set; }
+        public string sexo { get; set; }
+        public string telefone { get; set; }
+        public DateTime Datanasci { get; set; }
+
+        public string matricula  { get; set; }
+        public double  salario { get; set; }
+        public DateTime cargohorario { get; set; }
+        public string funcao { get; set; }
+     
+
+    }
+}
---
+++ trabalho/Pessoa .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho
+{
+    internal class Pessoa
+    {
+        public string nome { get; set; }
+        public string endererco  { get; set; }
+        public string cpf  { get; set; }
+        public string sexo  { get; set; }
+        public string telefone  { get; set; }
+
+
+    }
+}
